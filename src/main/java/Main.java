5cafdd54305ddcf1e8064bf51671f267fba9e937
Wessel <w.j.becker@st.hanze.nl>
2016-09-09 23:56:55 +0200

    ADD: CLI

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,18 +8,22 @@ public class Main {
         CommandLineParser parser = new BasicParser();
         Options options = new Options();
         options.addOption("d", "decrypt", false, "Sets the mode of the program to decrypting");
-        options.addOption("o", "original", false, "Sets the mode of the program to keeping characters that can not be encrypted from the input in the output");
+        options.addOption("o", "original", false,
+                "Sets the mode of the program to keeping characters that can not be encrypted from the input in the output");
         options.addOption("h", "help", false, "Display this help page");
 
         try {
             CommandLine commandLine = parser.parse(options, args);
 
-
             Controller controller = new Controller(commandLine);
             controller.run();
         } catch (ParseException e) {
+            System.out.println("Incorrect arguments:");
 //            print help page
-            System.out.println();
+            HelpFormatter helpFormatter = new HelpFormatter();
+            helpFormatter.printHelp("[-h] [-o] [-d] <key>",
+                    "This application will take the standard input and encrypt or decrypt it using a substitution cypher.",
+                    options, "");
         }
     }
 }
